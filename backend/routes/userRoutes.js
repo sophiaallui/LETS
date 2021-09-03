f04af142c1ca0e601114eeaf1d1108669124d7f7
Jae Cho <jc960116@gmail.com>
2021-09-03 08:20:43 -0700

    update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,4 +5,13 @@
 const jsonschema = require("jsonschema");
 const express = require("express");
 const router = express.Router();
-const { ensureCorrectUserOrAdmin, ensureAdmin } = require("")
+const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+const User = require("../models/user");
+/**
+ * POST  / { user } => { user, token } 
+ * Adds a new user. This is not the registration endpoint -- instead, this is 
+ * only for admin users to add new users. The new user being added can be an admin.
+ * 
+ * This returns the newly created user and an authentication token for them:
+ *  { user : { usernan}}
+ */
