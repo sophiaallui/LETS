33586c9a8467f13d814ecb33569764c712c4c6b3
Charles Park <charlieboyee@gmail.com>
2021-10-13 11:12:48 -0700

    Added hide function onblur

--- frontend/src/MyComponents/Search.js
+++ frontend/src/MyComponents/Search.js
@@ -1,134 +1,148 @@
 import React, { useState, useEffect } from 'react';
 import useDebounce from 'hooks/useDebounce';
 import {
-    Form,
-    FormGroup,
-    InputGroup,
-    InputGroupAddon,
-    InputGroupText,
-    Input,
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    ListGroup,
-    ListGroupItem,
-    Spinner,
-    Row,
-    Col,
+	Form,
+	FormGroup,
+	InputGroup,
+	InputGroupAddon,
+	InputGroupText,
+	Input,
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	ListGroup,
+	ListGroupItem,
+	Spinner,
+	Row,
+	Col,
 } from 'reactstrap';
 import Api from 'api/api';
 import { Link } from 'react-router-dom';
 import './searchDesign.css';
 
 const Search = (props) => {
-    const [ searchTerm, setSearchTerm ] = useState('');
-    const [ results, setResults ] = useState(null);
-    const [ isSearching, setIsSearching ] = useState(false);
-    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [results, setResults] = useState(null);
+	const [isSearching, setIsSearching] = useState(false);
+    const [hideSearchResults, setHideSearchResults] = useState(false);
+	const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-    useEffect(() => {
-        const handleSearch = async () => {
-            try {
-                setIsSearching(true);
-                const res = await Api.request(
-                    `users?username=${debouncedSearchTerm}&firstName=${debouncedSearchTerm}&lastName=${debouncedSearchTerm}&email=${debouncedSearchTerm}`
-                );
-                const users = res.users;
-                setResults(users);
-            } catch (e) {
-                console.error(e);
-            }
-        };
+	useEffect(() => {
+		const handleSearch = async () => {
+			try {
+				setIsSearching(true);
+				const res = await Api.request(
+					`users?username=${debouncedSearchTerm}&firstName=${debouncedSearchTerm}&lastName=${debouncedSearchTerm}&email=${debouncedSearchTerm}`
+				);
+				const users = res.users;
+				setResults(users);
+			} catch (e) {
+				console.error(e);
+			}
+		};
 
-        if (debouncedSearchTerm) {
-            setIsSearching(true);
-            handleSearch();
-        } else {
-            setResults(null);
-            setIsSearching(false);
-        }
-    }, [ debouncedSearchTerm ]);
+		if (debouncedSearchTerm) {
+			setIsSearching(true);
+			handleSearch();
+		} else {
+			setResults(null);
+		}
+	}, [debouncedSearchTerm]);
 
-    const handleChange = async (e) => {
-        setSearchTerm(e.target.value);
-        console.log(debouncedSearchTerm);
-    };
-    console.debug('results', results);
-    
-    return (
-        <>
-            <InputGroup>
-                <InputGroupAddon addonType='prepend'>
-                    <InputGroupText>
-                        <i className='fas fa-search' />
-                    </InputGroupText>
-                </InputGroupAddon>
-                <Input
-                    placeholder='Search'
-                    type='text'
-                    value={searchTerm}
-                    onChange={handleChange}
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        console.log(searchTerm);
-                    }}
-                />
-                <InputGroupAddon addonType='append'>
-                    <InputGroupText>
-                        {isSearching ? (
-                            <Spinner
-                                className='main-search'
-                                size='sm'
-                                color='primary'
-                            />
-                        ) : (
-                            <i
-                                onClick={() => {
-                                    console.log('x clicked');
-                                    setSearchTerm('');
-                                    
-                                }}
-                                className='ni ni-fat-remove'
-                            />
-                        )}
-                    </InputGroupText>
-                </InputGroupAddon>
-            </InputGroup>
-            <div className='row-container'>
-                <div className='inner-row-container'>
-                {results?
-                    results.map((user) => (
-                        <Row className='search-result' key={user.username}>
-                            <Col className='img'>
-                                <Link to={`/profile/${user.username}`}>
-                                    <img
-                                        alt='...'
-                                        src={
-                                            user.profileImage
-                                                ? user.profileImage
-                                                : require('assets/img/placeholder.jpg')
-                                        }
-                                    />
-                                </Link>
-                            </Col>
-                            <Col className='name-column'>
-                                <h5>
-                                    <a
-                                        href='#'
-                                        onClick={(e) => e.preventDefault()}
-                                    >
-                                        {user.firstName} {user.lastName}
-                                    </a>
-                                    <small>{user.username}</small>
-                                </h5>
-                            </Col>
-                        </Row>
-                    )):null
-                }
-                </div>
-            </div>
-        </>
-    );
+	useEffect(() => {
+		if (results) {
+			setIsSearching(false);
+		}
+	}, [results]);
+
+	const handleChange = async (e) => {
+		setSearchTerm(e.target.value);
+		console.log(debouncedSearchTerm);
+	};
+	console.debug('results', results);
+
+	return (
+		<>
+			<InputGroup>
+				<InputGroupAddon addonType='prepend'>
+					<InputGroupText>
+						<i className='fas fa-search' />
+					</InputGroupText>
+				</InputGroupAddon>
+				<Input
+					placeholder='Search'
+					type='text'
+					value={searchTerm}
+					onChange={handleChange}
+                    onFocus={()=>setHideSearchResults(false)}
+                    onBlur={()=>setHideSearchResults(true)}
+				/>
+				<InputGroupAddon addonType='append'>
+					<InputGroupText>
+						{isSearching ? (
+							<Spinner
+								className='main-search'
+								size='sm'
+								color='primary'
+							/>
+						) : (
+							<i
+								onClick={() => {
+									console.log('x clicked');
+									setSearchTerm('');
+								}}
+								className='ni ni-fat-remove'
+							/>
+						)}
+					</InputGroupText>
+				</InputGroupAddon>
+			</InputGroup>
+			<div className={`${hideSearchResults?'hide-row-container':'row-container'}`}>
+				<div className='inner-row-container'>
+					{results ? (
+						results.length == 0 ? (
+							<Row className='no-match'>
+								<Col>No match</Col>
+							</Row>
+						) : (
+							results.map((user) => (
+								<Row
+									className='search-result'
+									key={user.username}
+								>
+									<Col className='img'>
+										<Link to={`/profile/${user.username}`}>
+											<img
+												alt='...'
+												src={
+													user.profileImage
+														? user.profileImage
+														: require('assets/img/placeholder.jpg')
+												}
+											/>
+										</Link>
+									</Col>
+									<Col className='name-column'>
+										<h5>
+											<a
+												href='#'
+												onClick={(e) =>
+													e.preventDefault()
+												}
+											>
+												{user.firstName} {user.lastName}
+											</a>
+											<small>{user.username}</small>
+										</h5>
+									</Col>
+								</Row>
+							))
+						)
+					) : null}
+				</div>
+			</div>
+		</>
+	);
 };
 export default Search;
