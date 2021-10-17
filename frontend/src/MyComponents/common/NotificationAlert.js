f2a3c1cf63647b929d322ded4aad5b6d2b116507
Jae Pil Cho <48457791+cjp0116@users.noreply.github.com>
2021-10-17 00:16:35 -0700

    update

--- frontend/src/MyComponents/common/NotificationAlert.js
+++ frontend/src/MyComponents/common/NotificationAlert.js
@@ -2,10 +2,6 @@ import React from "react";
 // react plugin for creating notifications over the dashboard
 import NotificationAlert from "react-notification-alert";
 // reactstrap components
-import {
-  Button,
-} from "reactstrap";
-
 class Notifications extends React.Component {
   props = {
     title : "",
