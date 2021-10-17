e19c2efd77e6b9aa30b3fe5a8af969a5ffac1f58
Charles Park <charlieboyee@gmail.com>
2021-10-17 05:09:09 -0700

    Fixed post comment image alignment.

--- frontend/src/MyComponents/comment/Comment.js
+++ frontend/src/MyComponents/comment/Comment.js
@@ -1,36 +1,39 @@
-import React from "react";
-import {  Media } from "reactstrap";
-import { Link } from "react-router-dom";
-import { format} from "timeago.js"
+import React from 'react';
+import { Media } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { format } from 'timeago.js';
+import './commentDesign.css';
 const Comment = ({ comment }) => {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  return (
-    <Media className="media-comment">
-      <Link to={`/profile/${comment.postedBy}`}>
-        <img
-          alt="..."
-          className="media-comment-avatar rounded-circle"
-          src={
-            comment.commentorProfileImage
-              ? PF + comment.commentorProfileImage
-              : require("assets/img/faces/team-1.jpg")
-          }
-        />
-      </Link>
+	const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+	return (
+		<Media>
+			<Media className='media-comment'>
+				<Link to={`/profile/${comment.postedBy}`}>
+					<div className='comment-user-image-container'>
+						<img
+							alt='...'
+							src={
+								comment.commentorProfileImage
+									? PF + comment.commentorProfileImage
+									: require('assets/img/faces/team-1.jpg')
+							}
+						/>
+					</div>
+				</Link>
+			</Media>
 
-      <Media>
-        <div className="media-comment-text">
-          <h6 className="h5 mt-0">{comment.postedBy}</h6>
-          <p className="text-sm lh-160">{comment.content}</p>
-          <div className="icon-actions">
-            <small className="d-block text-muted">
-              {format(comment.createdAt)}
-            </small>
-          </div>
-        </div>
-      </Media>
-    </Media>
-  );
+			<Media body>
+				<Media heading>
+                    {comment.postedBy}
+                </Media>
+                <Media>
+                {comment.content}
+                </Media>
+				
+				{format(comment.createdAt)}
+			</Media>
+		</Media>
+	);
 };
 
 export default Comment;
